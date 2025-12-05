ce59fd9c949ce0f3cd8fd2e50b726915a331f6ab
rik7614 <dangolrikesh@gmail.com>
2025-12-05 16:14:09 +0000

    Update greeting message to test CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from DevOps Lab running in Kubernetes via Jenkins!"
+    return "Hello Rikesh from DevOps Lab running in Kubernetes via Jenkins!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
